f1b53775b7f3157446975c48c8d7c089774d5171
Sean Law <sean.law@alcolizer.com>
2017-09-18 16:42:41 +1000

    Place each component in its own js file

---
+++ src/components/SimilarPlayersCard.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PlayerAvatar from './PlayerAvatar.js'
+
+export default function SimilarPlayersCard(props) {
+
+  const similarPlayersList = [
+    {
+      'playerName': props.similarPlayer0,
+      'img': props.img0,
+      'ppg': props.ppg0,
+      'apg': props.apg0,
+      'rpg': props.rpg0
+    },
+    {
+      'playerName': props.similarPlayer1,
+      'img': props.img1,
+      'ppg': props.ppg1,
+      'apg': props.apg1,
+      'rpg': props.rpg1
+    },
+    {
+      'playerName': props.similarPlayer2,
+      'img': props.img2,
+      'ppg': props.ppg2,
+      'apg': props.apg2,
+      'rpg': props.rpg2
+    },
+    {
+      'playerName': props.similarPlayer3,
+      'img': props.img3,
+      'ppg': props.ppg3,
+      'apg': props.apg3,
+      'rpg': props.rpg3
+    },
+  ];
+
+  const playerAvatarList = similarPlayersList.map((player) =>
+    <PlayerAvatar
+      key={player.playerName}
+      playerName={player.playerName} img={player.img}
+      ppg={player.ppg} apg={player.apg} rpg={player.rpg}
+    />
+  );
+
+  return (
+    <div>
+      <h3>Similar Players</h3>
+      {playerAvatarList}
+    </div>
+  );
+}
