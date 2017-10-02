18b5984fef8d988e8cba594d40bbe16cab6e5427
Sean Law <sean.law.321@gmail.com>
2017-10-02 10:17:37 +1000

    Fix styling issues (see detailed commit msg)
    Issues:
    * Set fgColor to '#FFF' instead of 'FFF'
    * Prevent profile img from overflowing the card

--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,7 @@ export const highlightColor = '#00FFF9';
 export const midlightColor = '#49BDCA';
 export const lowlightColor = '#1E5F6F';
 export const bgColor = '#424242';
-export const fgColor = 'FFF';
+export const fgColor = '#FFF';
 export const accentColor = '#676767';
 
 /* Typography */
